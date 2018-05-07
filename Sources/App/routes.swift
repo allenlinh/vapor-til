c4e5f0c18de9e94dd19cd5559e805a388d06306d
linheng7786 <heng.lin7786@gmail.com>
2018-05-07 13:39:07 +0800

    first commit

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -9,10 +9,41 @@ public func routes(_ router: Router) throws {
     router.get("hello") { req in
         return "Hello, world!"
     }
+    
+    router.get("hello", "vapor") { req -> String in
+        return "Hello Vapor!"
+    }
+    
+    router.get("hello", String.parameter) { req -> String in
+        let name = try req.parameters.next(String.self)
+        return "Hello, \(name)!"
+    }
+    
+//    router.post(InfoData.self, at: "info") { (req, data) -> String in
+//        return "Hello \(data.name)"
+//    }
+    
+    router.post(InfoData.self, at: "info") { (req, data) -> InfoResponse in
+        return InfoResponse(request: data)
+    }
+    
+    router.post("api", "acronyms") { (req) -> Future<Acronym> in
+        return try req.content.decode(Acronym.self).flatMap(to: Acronym.self, { (acronym)  in
+            return acronym.save(on: req)
+        })
+    }
 
     // Example of configuring a controller
-    let todoController = TodoController()
-    router.get("todos", use: todoController.index)
-    router.post("todos", use: todoController.create)
-    router.delete("todos", Todo.parameter, use: todoController.delete)
+//    let todoController = TodoController()
+//    router.get("todos", use: todoController.index)
+//    router.post("todos", use: todoController.create)
+//    router.delete("todos", Todo.parameter, use: todoController.delete)
+}
+
+struct InfoData: Content {
+    let name: String
+}
+
+struct InfoResponse: Content {
+    let request: InfoData
 }
